d191cb5b725dbb9b0fc40e08edeae3b1079e7d24
sfbatchelor <sfbatchelor@gmail.com>
2019-12-20 11:41:44 +0000

    ported previous sketches as image brushes

---
+++ 191215_ImageBrush_AmbientStreakWithLiveAndDiffViews/boid.js
@@ -0,0 +1,172 @@
+let id = 7;
+class Boid {
+
+    constructor(posX, posY, colour) {
+       let spread = 3;
+        this.position = createVector(posX, posY);
+        //this.position = createVector(random(dimX/-spread, dimX/spread)+dimX/2, random(dimY/-spread, dimY/spread) +dimY/2);
+        this.velocity = p5.Vector.random2D();
+        this.velocity.setMag(random(2, 5));
+        this.acceleration = createVector();
+        this.maxForce = 0.4;
+        this.maxSpeed = 1.;
+        this.id = id;
+        this.width  = 10000000000;//random(1000000, 1000000);
+        this.colour = colour;
+        id++;;
+    }
+
+
+    edges() {
+        if (this.position.x > 960) {
+            this.position.x = 0;
+        } else if (this.position.x < 0) {
+            this.position.x = 960;
+        }
+        if (this.position.y > 540) {
+            this.position.y = 0;
+        } else if (this.position.y < 0) {
+            this.position.y = 540;
+        }
+
+
+
+
+    }
+
+    align(boids) {
+        let steering = createVector();
+        let perception = 50;
+        let total = 0;
+        for (let other of boids) {
+            let d = dist(this.position.x, this.position.y, other.position.x, other.position.y);
+            if (other != this && d < 100) {
+                steering.add(other.velocity);
+                total++;
+            }
+        }
+        if (total > 0) {
+            steering.div(total);
+            steering.setMag(this.maxSpeed);
+            steering.sub(this.velocity);
+            steering.limit(this.maxForce);
+            return steering;
+        }
+        return steering;
+    }
+
+    cohesion(boids) {
+        let steering = createVector();
+        let perception = 50;
+        let total = 0;
+        for (let other of boids) {
+            let d = dist(this.position.x, this.position.y, other.position.x, other.position.y);
+            if (other != this && d < 100) {
+                steering.add(other.position);
+                total++;
+            }
+        }
+        if (total > 0) {
+            steering.div(total);
+            steering.sub(this.position);
+            steering.setMag(this.maxSpeed);
+            steering.sub(this.velocity);
+            steering.limit(this.maxForce);
+            return steering;
+        }
+        return steering;
+    }
+
+    separation(boids) {
+        let steering = createVector();
+        let perception = 20;
+        let total = 0;
+        for (let other of boids) {
+            let d = dist(this.position.x, this.position.y, other.position.x, other.position.y);
+            if (other != this && d < 100) {
+                let diff = p5.Vector.sub(this.position, other.position);
+                diff.div(d * d);
+                steering.add(diff);
+                total++;
+            }
+        }
+        if (total > 0) {
+            steering.div(total);
+            steering.setMag(this.maxSpeed);
+            steering.sub(this.velocity);
+            steering.limit(this.maxForce);
+            return steering;
+        }
+        return steering;
+    }
+
+
+
+    flock(boids) {
+        this.acceleration.mult(0);
+        let alignment = this.align(boids);
+        let cohesion = this.cohesion(boids);
+        let separation = this.separation(boids);
+
+
+        alignment.mult(alignmentSlider.value());
+        cohesion.mult(cohesionSlider.value());
+        separation.mult(separationSlider.value());
+
+        this.acceleration.add(alignment);
+        this.acceleration.add(cohesion);
+        this.acceleration.add(separation);
+
+    }
+
+    update() {
+        this.position.add(this.velocity);
+        this.velocity.add(this.acceleration);
+        this.velocity.limit(this.maxSpeed);
+    }
+
+    show() {
+   
+      let w = noise((this.id + (frameCount / 1000000)) * this.width );
+        w = map(w, 0, 1, 0, 60);
+
+        let a = noise((this.id + (frameCount * 40)) * this.width );
+        let alpha = map(a, 0, 1 , 0, 50);
+        stroke(255, alpha);
+        let numSamples = 20;
+        let circular = true;
+        let e = 2;
+        // render many translucent points around the central one
+        this.colour.setAlpha(alpha);
+        fill(this.colour);
+        noStroke();
+        beginShape(POINTS)
+        for (let i = 0; i < numSamples; i++) {
+            if (circular === true) {
+
+                let offset = p5.Vector.random2D();
+                let radius = random(0,w);
+                radius = pow(-radius, e);
+                offset.x *= radius;
+                offset.y *= radius;
+          
+                strokeWeight(6);
+                vertex(this.position.x + offset.x, this.position.y + offset.y)
+   
+            } else {
+                let xOffset = random(-w / 2, w / 2);
+                let yOffset = random(-w / 2, w / 2);
+                vertex(this.position.x + xOffset, this.position.y + yOffset)
+            }
+
+        }
+
+        endShape();
+
+    }
+
+
+
+
+
+}
---
+++ 191217_ImageBrush_StaticVariedCircles/sketch.js
@@ -0,0 +1,198 @@
+//Edited from: https://kylemcdonald.github.io/cv-examples/
+
+//capture
+var capture;
+var diffImage;
+var previousPixels;
+var captureW = 1920;
+var captureH = 1080;
+var recaptureBG = true;
+var canvasW = captureH; //portrait capture setup
+var canvasH = captureW;
+var total = 0;
+
+
+//graphics
+let alignmentSlider, cohesionSlider, separationSlider;
+flock = [];
+var pause = true;
+
+function setup() {
+    createCanvas(canvasW, canvasH, WEBGL);
+    capture = createCapture(VIDEO);
+    diffImage = createCapture(VIDEO);
+    capture.elt.setAttribute('playsinline', '');
+    capture.size(captureW, captureH);
+    capture.hide();
+    diffImage.hide();
+    setAttributes('alpha', 1);
+    background(0);
+
+
+
+
+    alignmentSlider = createSlider(0, 5, 1, 0.1);
+    cohesionSlider = createSlider(0, 5, 1, 0.1);
+    separationSlider = createSlider(0, 5, 1, 0.1);
+
+    respawnBoids();
+
+}
+
+function copyImage(src, dst) {
+    var n = src.length;
+    if (!dst || dst.length != n) dst = new src.constructor(n);
+    while (n--) dst[n] = src[n];
+    return dst;
+}
+
+function processCapture() {
+    capture.loadPixels();
+    if (recaptureBG) {
+        previousPixels = copyImage(capture.pixels, previousPixels);
+        recaptureBG = false;
+    }
+    total = 0;
+    if (capture.pixels.length > 0) { // don't forget this!
+        if (previousPixels) {
+            var w = capture.width,
+                h = capture.height;
+            var i = 0;
+            var thresholdAmount = select('#thresholdAmount').value() * 255. / 500.;
+            thresholdAmount *= 3; // 3 for r, g, b
+            diffImage = capture.get(0, 0, captureW, captureH);
+            diffImage.loadPixels();
+            for (var y = 0; y < h; y++) {
+                for (var x = 0; x < w; x++) {
+                    // calculate the differences
+                    var rdiff = Math.abs(capture.pixels[i + 0] - previousPixels[i + 0]);
+                    var gdiff = Math.abs(capture.pixels[i + 1] - previousPixels[i + 1]);
+                    var bdiff = Math.abs(capture.pixels[i + 2] - previousPixels[i + 2]);
+                    // copy the current pixels to previousPixels
+                    //                    previousPixels[i + 0] = pixels[i + 0];
+                    //                    previousPixels[i + 1] = pixels[i + 1];
+                    //                    previousPixels[i + 2] = pixels[i + 2];
+                    var diffs = rdiff + gdiff + bdiff;
+                    var output = 0;
+                    if (rdiff > thresholdAmount || gdiff > thresholdAmount || bdiff > thresholdAmount) {
+                        output = 255;
+                        total += diffs;
+                        i += 3;
+                    } else {
+                        diffImage.pixels[i++] = output;
+                        diffImage.pixels[i++] = 255;
+                        diffImage.pixels[i++] = output;
+                    }
+
+                    // also try this:
+                    // pixels[i++] = rdiff;
+                    // pixels[i++] = gdiff;
+                    // pixels[i++] = bdiff;
+                    i++; // skip alpha
+                }
+            }
+        }
+    }
+    // need this because sometimes the frames are repeated
+    diffImage.updatePixels();
+
+
+}
+
+function drawCapture() {
+
+    //draw capture
+    push();
+    translate(-captureH, 0);
+    rotate(radians(-90)); //rotate for portrait
+    texture(capture);
+    plane(captureW, captureH);
+    pop();
+
+
+}
+
+
+function drawDiffImage() {
+    //draw diff-image
+    push();
+    rotate(radians(-90)); //rotate for portrait
+    texture(diffImage);
+    plane(captureW, captureH);
+    pop();
+}
+
+function draw() {
+
+    select('#motion').elt.innerText = total;
+    processCapture();
+
+    push();
+    translate(-captureH / 2, height / 2);
+    rotate(radians(-90)); //rotate for portrait
+    for (let boid of flock) {
+        boid.edges();
+        if (pause === true) {
+            boid.flock(flock);
+            boid.update();
+            boid.show();
+        }
+    }
+
+    pop();
+
+
+
+}
+
+
+function respawnBoids() {
+
+    flock = [];
+    if (diffImage.width > 0) {
+        diffImage.loadPixels();
+        for (let i = 0; i < 5; i++) {
+            for (let j = 0; j < 5; j++) {
+                let x = int(random(0, diffImage.width));
+                let y = int(random(0, diffImage.height));
+                let index = (x + y * diffImage.width) * 4
+                while (diffImage.pixels[index] === 0 && diffImage.pixels[index + 1] === 255 && diffImage.pixels[index + 2] === 0) {
+                    x = int(random(0, diffImage.width));
+                    y = int(random(0, diffImage.height));
+                    index = (x + y * diffImage.width) * 4
+                }
+                flock.push(new Boid(x, y, color(diffImage.pixels[index], diffImage.pixels[index + 1], diffImage.pixels[index + 2])));
+                console.log('captured', x, y);
+            }
+        }
+    } else {
+
+        for (let i = 0; i < 30; i++) {
+            flock.push(new Boid(random(-captureW, captureW) / 2, random(-captureH, captureH) / 2, color(255, 0, 0)));
+        }
+    }
+}
+
+function keyReleased() {
+    if (key === 'X') {
+        recaptureBG = true;
+    }
+    if (keyCode === ENTER) {
+        m = month();
+        d = day();
+        y = year();
+        h = hour();
+        mi = minute();
+        se = second();
+        save(y + '.' + m + '.' + d + '_' + h + '.' + mi + '.' + se + '.png');
+    }
+    if (key === 'R') {
+        respawnBoids();
+    }
+    if (key === ' ') {
+        pause = !pause;
+    } else if (keyCode === SHIFT) {
+        background(0);
+    }
+
+}
